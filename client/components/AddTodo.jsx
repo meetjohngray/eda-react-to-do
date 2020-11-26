34a688a3483ac86226a3ed94a54e461b646252e1
jgray1 <john.edwin.gray@gmail.com>
2020-11-27 11:55:26 +1300

    Complete filter functionality

--- client/components/AddTodo.jsx
+++ client/components/AddTodo.jsx
@@ -21,13 +21,13 @@ class AddTodo extends React.Component {
       // Add the task to the db
       apiAddTask(this.state)
         // We need the id so that after we add the task, the edit task form doesn't fire
-         .then((id) => {
+        .then((id) => {
           //  We turn our info into an object...
-           const taskObj = {
-             task: this.state.task,
-             id: id
-           }
-            // And give this object to our action to change the current state to reflect the changes
+          const taskObj = {
+            task: this.state.task,
+            id: id
+          }
+          // And give this object to our action to change the current state to reflect the changes
           this.props.dispatch(addTask(taskObj))
           // Reset the form to an empty string
           this.setState({ task: ''})
--- client/components/TaskList.jsx
+++ client/components/TaskList.jsx
@@ -6,15 +6,13 @@ import { initTask, deleteTask } from '../actions/index'
 
 class TaskList extends React.Component {
   state = {
-    listState: 'all',
-    displayList: []
+    filter: 'all'
   }
 
   componentDidMount () {
     getAllTasks()
       .then(task => {
         this.props.dispatch(initTask(task))
-        this.chooseList()
       })
       .catch(err => console.log(err))
   }
@@ -33,32 +31,11 @@ class TaskList extends React.Component {
     )
   }
 
-  chooseList = () => {
-    const completedTasks =  this.props.tasks.filter(task => task.isComplete)
-    const uncompletedTasks =  this.props.tasks.filter(task => !task.isComplete)
-    // eslint-disable-next-line no-unused-vars
-    let displayList
-    if (this.state.listState == 'all') {
-      this.setState({ displayList: this.props.tasks })
-    } else if (this.state.listState == 'completed') {
-      this.setState({ displayList: completedTasks })
-    } else if (this.state.listState == 'active') {
-      this.setState({ displayList:uncompletedTasks })
-    }
-    return this.state.displayList
-  }
-
-  // Pass a callback into setState because
-  // it DOES not update state instantaneously
-  // The callback makes it wait to get the proper
-  // displayList until AFTER setState is complete
-  stateClick = (newState) => {
+  filterClick = (event, filter) => {
+    event.preventDefault()
     this.setState({
-      listState: newState
-      // Just chooseList without the ()
-      // When the () is present, the results
-      // of chooseList (an array of objects) is returned
-    }, this.chooseList)
+      filter: filter
+    })
   }
 
   render () {
@@ -71,12 +48,21 @@ class TaskList extends React.Component {
             {/* <!-- These are here just to show the structure of the list items --> */}
             {/* <!-- List items should get the class `editing` when editing and `completed` when marked as completed --> */}
 
-            {this.state.displayList.map(task => {
-              return (
-              // Once I pass this over, I need to refer to it with this.state.props
-                <ListItem task={task}/>
-              )
-            })}
+            {this.props.tasks.filter(task => {
+              switch (this.state.filter) {
+                case 'active':
+                  return !task.isComplete
+                case 'completed':
+                  return task.isComplete
+                default:
+                  return true
+              }
+            })
+              .map(task => {
+                // Once I pass this over, I need to refer to it with this.state.props
+                return <ListItem task={task}/>
+              })
+            }
           </ul>
         </section>
 
@@ -87,13 +73,13 @@ class TaskList extends React.Component {
           <ul className="filters">
             {/* onClick={()=>this.handleClick(this.props.task.id)} */}
             <li>
-              <a className="selected" href="#/" onClick={() => this.stateClick('all')}>All</a>
+              <a className="selected" href="#/" onClick={ event => this.filterClick(event, 'all')}>All</a>
             </li>
             <li>
-              <a href="#/active" onClick={() => this.stateClick('active')}>Active</a>
+              <a href="#/active" onClick={ event => this.filterClick(event, 'active')}>Active</a>
             </li>
             <li>
-              <a href="#/completed" onClick={() => this.stateClick('completed')}>Completed</a>
+              <a href="#/completed" onClick={ event => this.filterClick(event, 'completed')}>Completed</a>
             </li>
           </ul>
           {/* <!-- Hidden if no completed items are left ↓ --> */}
@@ -104,11 +90,40 @@ class TaskList extends React.Component {
   }
 }
 
-function mapStateToProps(globalState) {
+function mapStateToProps (globalState) {
   return { tasks: globalState.tasks }
 }
 
 export default connect(mapStateToProps)(TaskList)
+
+// chooseList = () => {
+//   const completedTasks = this.props.tasks.filter(task => task.isComplete)
+//   const uncompletedTasks = this.props.tasks.filter(task => !task.isComplete)
+//   const allTasks = this.props.tasks
+//   // eslint-disable-next-line no-unused-vars
+//   let displayList
+//   if (this.state.listState == 'all') {
+//     this.setState({ displayList: allTasks })
+//   } else if (this.state.listState == 'completed') {
+//     this.setState({ displayList: completedTasks })
+//   } else if (this.state.listState == 'active') {
+//     this.setState({ displayList: uncompletedTasks })
+//   }
+//   return this.state.displayList
+// }
+
+// Pass a callback into setState because
+// it DOES not update state instantaneously
+// The callback makes it wait to get the proper
+// displayList until AFTER setState is complete
+// filterClick = (newState) => {
+//   this.setState({
+//     listState: newState
+// Just chooseList without the ()
+// When the () is present, the results
+// of chooseList (an array of objects) is returned
+//   }, this.chooseList)
+// }
 
 { /* <li class="completed">
           <div class="view">
