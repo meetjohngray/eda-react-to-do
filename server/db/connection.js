2ec3256d892a39cf9fabd17892a37b14c96b0f9f
jgray1 <john.edwin.gray@gmail.com>
2020-12-01 11:40:27 +1300

    fix db connection issue

--- server/db/connection.js
+++ server/db/connection.js
@@ -10,36 +10,3 @@ const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
 module.exports = connection
-
-function getToDos (db = connection) {
-  return db('toDos').select()
-}
-
-function addToDo (newTask, db = connection) {
-  return db('todos')
-    .insert(
-      {
-        task: newTask.task,
-        priority: newTask.priority,
-        details: newTask.details,
-        isComplete: false
-      })
-}
-
-function deleteToDo( id,db = connection ) {
-  return db('todos').where('id', id )
-  .del()
-}
-
-function updateToDo ( id, updatedToDo, db = connection ) {
-  console.log("updatedToDoDB")
-  return db('todos').where('id', id)
-    .update(
-      {
-        task: updatedToDo.task,
-        priority: updatedToDo.priority,
-        details: updatedToDo.details,
-        isComplete: updatedToDo.isComplete
-      })
-}
-module.exports = connection
--- server/db/todos.js
+++ server/db/todos.js
@@ -21,7 +21,6 @@ function deleteToDo (id, db = connection) {
 }
 
 function updateToDo (id, updatedToDo, db = connection) {
-  console.log('updatedToDoDB')
   return db('todos').where('id', id)
     .update(
       {
