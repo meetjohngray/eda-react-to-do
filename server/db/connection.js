8e084b4eef7448679ee282511634d1360c2e9325
jgray1 <john.edwin.gray@gmail.com>
2020-12-01 11:24:24 +1300

    pull toDo db functions out of connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -3,36 +3,4 @@ const config = require('./knexfile')
 const env = process.env.NODE_ENV || 'development'
 const connection = knex(config[env])
 
-module.exports = { getToDos, addToDo, deleteToDo, updateToDo }
-
-function getToDos ( db = connection ) {
-  return db('toDos').select()
-}
-
-function addToDo ( newTask,db = connection ) {
-  return db('todos')
-    .insert(
-      {
-        task: newTask.task,
-        priority: newTask.priority,
-        details: newTask.details,
-        isComplete: false
-      })
-}
-
-function deleteToDo( id,db = connection ) {
-  return db('todos').where('id', id )
-  .del()
-}
-
-function updateToDo ( id, updatedToDo, db = connection ) {
-  console.log("updatedToDoDB")
-  return db('todos').where('id', id)
-    .update(
-      {
-        task: updatedToDo.task,
-        priority: updatedToDo.priority,
-        details: updatedToDo.details,
-        isComplete: updatedToDo.isComplete
-      })
-}
+module.exports = connection
