f0c44ed3aac51ef228d6def981252cdaa1d07da3
jgray1 <john.edwin.gray@gmail.com>
2021-01-28 10:19:45 +1300

    write tests for /actions/index.js

--- client/actions/index.test.js
+++ client/actions/index.test.js
@@ -1,5 +1,29 @@
-// import { addTask } from './index'
+import { addTask, ADD_TASK, updateTask, UPDATE_TASK } from './index'
 
-// describe('addTask', () => {
-//   test('Do the thing')
-// })
+describe('actions', () => {
+  test('Is working', () => {
+    const expected = 1
+    const actual = 1
+    expect(expected).toBe(actual)
+  })
+
+  test('should create an action to add a task', () => {
+    const task = 'Write a test'
+    const expectedAction = {
+      type: ADD_TASK,
+      task
+    }
+    expect(addTask(task)).toEqual(expectedAction)
+  })
+
+  test('should create an action to update a task', () => {
+    const id = 1
+    const task = 'Update a test'
+    const expectedAction = {
+      type: UPDATE_TASK,
+      id,
+      task
+    }
+    expect(updateTask(id, task)).toEqual(expectedAction)
+  })
+})
