4fd4aa90148bc917cb03d14880705691de2fa37f
jgray1 <john.edwin.gray@gmail.com>
2021-03-03 16:09:13 +1300

    test updateTask

--- client/reducers/tasks.test.js
+++ client/reducers/tasks.test.js
@@ -1,5 +1,5 @@
 import tasksReducer from './tasks'
-import { INIT_TASK, ADD_TASK, DEL_TASK, UPDATE_TASK } from '../actions'
+import { initTask, addTask, deleteTask, UPDATE_TASK } from '../actions'
 
 describe('tasks reducer', () => {
   test('sets array as initial state', () => {
@@ -11,21 +11,21 @@ describe('tasks reducer', () => {
 
   test('INIT_TASK', () => {
     const task = ['anything']
-    const action = { type: INIT_TASK, task }
+    const action = initTask(task)
     const state = tasksReducer(undefined, action)
     expect(state).toEqual(task)
   })
 
   test('ADD_TASK', () => {
-    const task = 'A new thing'
-    const action = { type: ADD_TASK, task }
+    const task = { task: 'A new thing' }
+    const action = addTask(task)
     const state = tasksReducer(['a task', 'another task'], action)
     expect(state).toHaveLength(3)
   })
 
   test('DEL_TASK', () => {
     const initialTasks = [{ id: 1, task: 'a task' }, { id: 4, task: 'another task' }]
-    const action = { type: DEL_TASK, id: 4 }
+    const action = deleteTask(4)
     const state = tasksReducer(initialTasks, action)
     expect(state).toHaveLength(1)
     expect(state[0].id).toBe(1)
@@ -33,13 +33,11 @@ describe('tasks reducer', () => {
 
   test('UPDATE_TASK', () => {
     const initialTasks = [{ id: 1, task: 'a task' }, { id: 4, task: 'another task' }]
-    const updatedTaskId = 1
-    const updatedTask = 'a different task'
-    const action = { type: UPDATE_TASK, id: updatedTaskId, task: updatedTask }
+    // const updatedTask = { id: 1, task:'a different task' }
+    const action = { type: UPDATE_TASK, id: 1, task: 'a different task' }
     const state = tasksReducer(initialTasks, action)
     expect(state).toHaveLength(2)
-    console.log(state[0])
     expect(state[0].id).toBe(1)
-    expect(state[0].task).toEqual(updatedTask)
+    expect(state[0].task).toEqual('a different task')
   })
 })
