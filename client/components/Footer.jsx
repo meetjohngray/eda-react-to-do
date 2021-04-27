3011e3c0230b9d220ff3f6a928f4279c5f8daa02
John Gray <john.edwin.gray@gmail.com>
2021-04-28 10:55:14 +1200

    convert Footer.jsx to hooks

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer className="info">
-       <p>Double-click to edit a todo</p>
-       <p>Created by <a href="https://meetjohngray.com">you</a></p>
-       <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-     </footer>
-    )
-  }
+const Footer = () => {
+  return (
+    <footer className="info">
+      <p>Double-click to edit a todo</p>
+      <p>Created by <a href="https://meetjohngray.com">you</a></p>
+      <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+    </footer>
+  )
 }
 
 export default Footer
