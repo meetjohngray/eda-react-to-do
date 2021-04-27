539d0b9c7c8e550f019b165f806ffe95231ad32e
John Gray <john.edwin.gray@gmail.com>
2021-04-28 11:05:29 +1200

    update Header.jsx for consistency of formatting

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -2,21 +2,19 @@ import React from 'react'
 import AddTodo from './AddTodo'
 import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
 
-class Header extends React.Component {
-  render () {
-    return (
-      <>
-        <header className="header">
-          <h1>todos</h1>
-          <IfAuthenticated>
-            <AddTodo/>
-          </IfAuthenticated>
-          <IfNotAuthenticated>
-          </IfNotAuthenticated>
-        </header>
-      </>
-    )
-  }
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <h1>todos</h1>
+        <IfAuthenticated>
+          <AddTodo/>
+        </IfAuthenticated>
+        <IfNotAuthenticated>
+        </IfNotAuthenticated>
+      </header>
+    </>
+  )
 }
 
 export default Header
