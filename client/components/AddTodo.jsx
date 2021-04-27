b7d47ed12a1898a982da8e6daa07f687a1bac90c
John Gray <john.edwin.gray@gmail.com>
2021-04-28 11:51:39 +1200

    update AddToDo.jsx to hooks

--- client/components/AddTodo.jsx
+++ client/components/AddTodo.jsx
@@ -1,55 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { apiAddTask } from '../apis/index'
 import { addTask } from '../actions/index'
 
-class AddTodo extends React.Component {
+const AddTodo = (props) => {
   // Set the initial state of task
-  state = {
-    task: ''
-  }
+  // state = {
+  //   task: ''
+  // }
 
-  handleChange = (event) => {
+  const [task, setTask] = useState('')
+
+  const handleChange = (event) => {
     // Get the change from the input field and put it in task state
-    this.setState({ task: event.target.value })
+    setTask(event.target.value)
   }
 
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault()
     // Add the task to the db
-    apiAddTask(this.state)
+    apiAddTask({ task: task })
     // We need the id so that after we add the task, the edit task form doesn't fire
       .then((id) => {
         //  We turn our info into an object...
         const taskObj = {
-          task: this.state.task,
+          task: task,
           id: id
         }
         // And give this object to our action to change the current state to reflect the changes
-        this.props.dispatch(addTask(taskObj))
+        props.dispatch(addTask(taskObj))
         // Reset the form to an empty string
-        this.setState({ task: '' })
+        setTask('')
         return null
       })
       .catch(err => {
         console.log(err)
       })
   }
-
-  render () {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          className="new-todo"
-          placeholder="What needs to be done?"
-          autoFocus={true}
-          value={this.state.task}
-          onChange={this.handleChange}
-          // onKeyDown={this.handleSubmit}
-        />
-      </form>
-    )
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        className="new-todo"
+        placeholder="What needs to be done?"
+        autoFocus={true}
+        value={task}
+        onChange={handleChange}
+        // onKeyDown={this.handleSubmit}
+      />
+    </form>
+  )
 }
 
 export default connect()(AddTodo)
